09bab2d21c64a7c55117d290c69fc85d7948f600
Rob Chung <chung.robert@hotmail.com>
2022-04-19 19:02:53 -0400

    Initial Commit

---
+++ TripTrack_LoginWeather/TripTrack_LoginWeather/View Controllers/ViewController.swift
@@ -0,0 +1,33 @@
+//
+//  ViewController.swift
+//  TripTrack_LoginWeather
+//
+//  Created by Rob C on 2022-04-18.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    let API_KEY = "ij8qxK4AOxpnXGsz4DgDfQLpjjllUFKw"
+    
+    @IBOutlet var lblCity: UILabel?
+    @IBOutlet var lblTemp: UILabel?
+    @IBOutlet var lblHumidity: UILabel?
+    @IBOutlet var lblWind: UILabel?
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        
+    }
+
+    
+    func getLocationKey() {
+        
+        
+    }
+
+}
+
